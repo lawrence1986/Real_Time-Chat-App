35021e530d02635a55a720aa87c1421fd2dd24f5
codeabuu <abdulngui718@gmail.com>
2023-11-27 18:23:07 -0500

    updates on user search

--- myproject/chat/urls.py
+++ myproject/chat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import messages_page, user_login, user_search, user_logout
+from .views import messages_page, user_login, user_search, start_chat, user_logout
 
 urlpatterns = [
     path('messages/', messages_page, name='messages'),
     path('user_login/', user_login, name='login'),
     path('user_search/', user_search, name='user_search'),
     path('user_logout/', user_logout, name='logout'),
-    # Other URL patterns
+    path('user_search/', user_search, name='user_search'),
+    path('start_chat/<int:user_id>/', start_chat, name='start_chat'),
 ]
--- myproject/chat/views.py
+++ myproject/chat/views.py
@@ -27,10 +27,38 @@ def user_login(request):
 
     return render(request, 'login.html', {'form': form})
 
-def user_search(request):
-    query = request.GET.get('q', '')
-    users = User.objects.filter(username__icontains=query)
-    return render(request, 'user_search.html', {'users': users, 'query': query})
 def user_logout(request):
     logout(request)
     return redirect('login')
+
+
+def user_search(request):
+    if 'q' in request.GET:
+        query = request.GET['q']
+        users = User.objects.filter(username__icontains=query)
+        print(users)
+    else:
+        users = User.objects.all()
+
+    context = {'users': users}
+    return render(request, 'messages.html', context)
+
+@login_required
+def start_chat(request, user_id):
+    # Get the other user based on user_id
+    other_user = get_object_or_404(User, id=user_id)
+
+    # Check if a thread already exists between the current user and the other user
+    thread = Thread.objects.filter(
+        participants=request.user
+    ).filter(participants=other_user).first()
+
+    if not thread:
+        # If no thread exists, create a new thread
+        thread = Thread.objects.create()
+        thread.participants.add(request.user, other_user)
+        thread.save()
+
+    # Redirect the user to the chat page with the thread_id
+    return redirect('chat_detail', thread_id=thread.id)
+
