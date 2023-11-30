42e78cfad7f7fb0d9c32a9ddfaa57e42300c6186
codeabuu <abdulngui718@gmail.com>
2023-11-30 08:28:19 -0500

    updates

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mpnri__+34v@j32el2yr6@9#pn_y#i9ub!a%)@otmx3%d$mh*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['100.25.155.56', '100.26.153.108', '127.0.0.1']
 
 
 # Application definition
